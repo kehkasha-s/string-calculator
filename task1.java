aaa9fe6f7c32b798dd26ec4d0db2856523c06faa
kehkasha-s <49408107+kehkasha-s@users.noreply.github.com>
2021-03-18 12:07:00 +0530

    task5
    task5:-changes done to handle exception of all kind and non negative numbers will be printed if they are present
    this code will ignore all faulty elements or numbers and give sum of valid numbers

--- task1.java
+++ task1.java
@@ -1,3 +1,16 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+// import java.util.*;
+
+class MyException extends Exception
+{
+ public MyException(String str)
+ {
+   super();
+ }
+}
+
 public class AddClass {
     
     //Add(string numbers) method to add 1 or 2 numbers and return 0 if string is empty
@@ -24,21 +37,52 @@ public class AddClass {
             
         //condition for adding unknown amount of numbers(use double type while addition and convert to int while printing output)
         else{
+            //flag varaible to keep track if negative number is there or not
+            int flag=0;
+            //list to store negative numbers to print in the message at the end
+            List<Double> arrlist = new ArrayList<Double>(); 
+  
+        
             double sum=0;
+          
             for (int i = 0; i < str_arr.length; i++){
+                double num=0;
                 //to handle exception
                 try{
-                sum =sum+Double.parseDouble(str_arr[i]);
-                }
+                    //parse number from string array
+                    num =Double.parseDouble(str_arr[i]);
+                    if(num<0)
+                        throw new MyException("Negatives not allowed");
+                    else
+                        sum =sum+num;
+                
+                    }
+                catch(MyException m){
+                    flag=1;
+                    // Add negative element to the list and continue execution of for loop
+                    arrlist.add(num); 
+                    continue;
+                    }
                 catch(Exception e){
-                    //break function if exception occured
+                    //break function if exception other than negative numbers occured
                     System.out.println("Exception occured while adding numbers");
-                    return 0;
-                    // break;
-                }
-                }
+                    continue; //if we want to simply ignore faulty number(which might contain other charachters) and other exception while adding particular number,
+                              //execute this line
+                    // return 0;
+                    // break;  //else execute this line
+                    }
+                //print message with list if non negative numbers are there
+                finally{
+                    if(i==str_arr.length-1 && flag==1){
+                    System.out.print("Negatives not allowed: ");
+                    // for(int j=0;j<arrlist.size();j++)
+                        System.out.println(arrlist);  //it will print numbers in double datatype, we change it to int if required
+                    }
+                 }
+            }
             
             int _sum=(int)sum;
+            System.out.print("sum: ");
             System.out.println(_sum);
    
             }
